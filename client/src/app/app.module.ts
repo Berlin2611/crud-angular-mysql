6e62d89da564d013629940aa4a5f806e2ffb9c15
Jorge Ulises Useche Cuellar <jusech5@bancodebogota.com.co>
2020-05-12 09:46:52 -0500

    Adding client

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GamesService } from './services/games.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    GameListComponent,
+    GameFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule, 
+    FormsModule
+  ],
+  providers: [GamesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
